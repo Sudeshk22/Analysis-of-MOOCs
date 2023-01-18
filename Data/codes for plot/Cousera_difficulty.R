dde2edf3373e9812112bd660d613bc09e003a28d
sudesh kumari <110173175+Sudeshk22@users.noreply.github.com>
2023-01-18 11:42:57 +0530

    Delete Data/codes for plot directory

--- Data/codes for plot/Cousera_difficulty.R
+++
@@ -1,7 +0,0 @@
-crsr = read.csv("merged_courses.csv")
-
-
-tab = table(crsr$Difficulty)
-tab = sort(tab, decreasng = FALSE)
-
-barplot(tab[c(4,2,1)], col = "brown")
--- Data/codes for plot/EdX_difficulty.R
+++
@@ -1,7 +0,0 @@
-edx = read.csv("edX_Course.csv")
-
-
-tab = table(edx$Level)
-tab = sort(tab, decreasng = FALSE)
-
-barplot(tab[14:12], col = "brown")
